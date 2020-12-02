540617096ef88bdbdb843c336e728e6adf4fea21
yasha6081 <yasha6081@gmail.con>
2020-12-02 09:45:04 -0600

    cookiekey session

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -7,6 +10,15 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+      cookieSession({
+            //for how long its gonna stay inside the server
+         maxAge: 30 * 24 * 60 * 60 * 1000
+           // key for cookey
+           keys:[keys.cookieKey]        
+      })
+);
 
 require("./routes/authRoutes")(app);
 
