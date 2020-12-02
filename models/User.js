0835bc3ac4c4d218d254222bc5f9a16c8c5ea0ce
yasha6081 <yasha6081@gmail.con>
2020-12-01 23:14:18 -0600

    finally connected to DB

---
+++ models/User.js
@@ -0,0 +1,9 @@
+//mongoose model class
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
