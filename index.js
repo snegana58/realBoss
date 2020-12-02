86c2ffead6c37dd5a6e10aa0d034616552097c05
yasha6081 <yasha6081@gmail.con>
2020-12-01 21:41:44 -0600

    mongoose added

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = ("./config/keys.js");
 require("./services/passpost");
  
+mongoose.connect(keys.mongoURI);
+
 
 const app = express();
 
 require("./routes/authRoutes")(app);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
