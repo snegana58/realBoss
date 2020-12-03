c51db0072fd1e708be0851246d50429fd4c5f982
yasha6081 <yasha6081@gmail.con>
2020-12-03 17:20:19 -0600

    survey btn

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,7 +8,7 @@ class Mailer extends helper.Mail {
     super();
 
     this.sgApi = sendgrid(keys.sendGridKey);
-    this.from_email = new helper.Email("yasha6081@gamil.com");
+    this.from_email = new helper.Email("yasha6081@gmail.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
