457300c2f0ee2c70fb27210a9799a493b35bddf9
yasha6081 <yasha6081@gmail.con>
2020-12-03 11:52:57 -0600

    stripe routes

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class Payments extends Component {
   render() {
     return (
       <StripeCheckout
-      name="Real Boss"
-      description="$5 is for 5 email credits"
+        name="Real Boss"
+        description="$5 is for 5 email credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
 
@@ -20,4 +22,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
