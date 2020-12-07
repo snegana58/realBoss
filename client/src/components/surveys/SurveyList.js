47dcba21771fb6b28427981361b1ce854ea83ef9
yasha6081 <yasha6081@gmail.con>
2020-12-07 11:58:07 -0600

    checking

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,56 +3,43 @@ import { connect } from "react-redux";
 import { fetchSurveys } from "../../actions";
 
 class SurveyList extends Component {
-      componentDidMount() {
-            this.props.fetchSurveys();
-      }
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
 
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+              {survey.body}
+            </p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes Vote: {survey.yes}</a>
+            <a>No Vote: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
 
-      renderSurveyss() {
-
-            return this.props.surveys.reverse().map(survey => {
-                  return (
-
-                        <div className="card darken-1" key={survey.id} >
-
-                              <div className="card-content">
-                                    <span className="card-title">{survey.title}</span>
-                                    <p>
-                                          {survey.body}
-                                    </p>
-                                    <p className="right">
-
-                                          Sent On :{new Date(survey.dateSent).toLocaleDateString()}
-                                    </p>
-                              </div>
-
-                             <div className="card-action">
-                             <a> Yes Votes: {survey.yes}</a>                     
-                             <a> No Votes: {survey.no}</a> 
-                                   
-
-                             </div>
-
-                        </div>
-                  );
-
-            });
-
-      }
-      render() {
-            return (
-                  <div>
-                        {this.renderSurveyss()}
-                  </div>
-            );
-
-      }
-
+  render() {
+    return (
+      <div>
+        {this.renderSurveys()}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps({ surveys }) {
-
-      return { surveys: surveys };
+  return { surveys };
 }
 
 export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
