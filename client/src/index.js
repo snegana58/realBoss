020fe503bb5f89481205784c9e2521e093f3f8ab
yasha6081 <yasha6081@gmail.con>
2020-12-07 19:38:14 -0600

    index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -8,10 +8,10 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+
 // axios helpers for Development better than Postman!!
 import axios from "axios";
 window.axios = axios;
-
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
