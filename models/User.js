bcc0f261a22985daf5de7e1ace39971748830346
yasha6081 <yasha6081@gmail.con>
2020-12-07 19:35:23 -0600

    deleting debug file

--- models/User.js
+++ models/User.js
@@ -1,11 +1,9 @@
-//mongoose model class
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
   credits: {type: Number, default: 0 }
-
 });
 
 mongoose.model('users', userSchema);
