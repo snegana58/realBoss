228e4f5bc4c1b81600b76259f287c4580a22746c
yasha6081 <yasha6081@gmail.con>
2020-12-07 19:47:44 -0600

    index back

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -20,10 +20,8 @@ export const submitSurvey = (values, history) => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const fetchSurveys = () => async dispatch =>  {
-    
+export const fetchSurveys = () => async dispatch => {
   const res = await axios.get("/api/surveys");
-   
-  dispatch({ type: FETCH_SURVEYS, payload: res.data })
 
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
